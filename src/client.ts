d6753a1448c4e9e12534e1618feedb8367818377
ZiomaleQ <r.partyka30@gmail.com>
2020-11-17 16:32:04 +0100

    get tweets

--- src/client.ts
+++ src/client.ts
@@ -37,6 +37,20 @@ export class Twino {
         return (await temp.json()).data[0] as TweetType
     }
 
+    async getTweetsWithOptions(id: string[], options: Fields) {
+        var optionsString = "";
+        if (options.all) options = { expansions: true, media: true, place: true, poll: true, tweet: true, user: true }
+        if (options.expansions) optionsString += "&expansions=attachments.poll_ids,attachments.media_keys,author_id,entities.mentions.username,geo.place_id,in_reply_to_user_id,referenced_tweets.id,referenced_tweets.id.author_id"
+        if (options.media) optionsString += "&media.fields=duration_ms,height,media_key,preview_image_url,type,url,width,public_metrics,promoted_metrics"
+        if (options.place) optionsString += "&place.fields=contained_within,country,country_code,full_name,geo,id,name,place_type"
+        if (options.poll) optionsString += "&poll.fields=duration_minutes,end_datetime,id,options,voting_status"
+        if (options.tweet) optionsString += "&tweet.fields=attachments,author_id,context_annotations,conversation_id,created_at,entities,geo,id,in_reply_to_user_id,lang,public_metrics,possibly_sensitive,referenced_tweets,source,text,withheld"
+        if (options.user) optionsString += "&user.fields=created_at,description,entities,id,location,name,pinned_tweet_id,profile_image_url,protected,public_metrics,url,username,verified,withheld"
+
+        var temp = await this._fetch<Response>("GET", `tweets?ids=${id.join(",")}${optionsString}`, "", false)
+        return (await temp.json()).data.map((x: any) => x as TweetType)
+    }
+
     async _fetch<T>(method: string, path: string, body: string | FormData | null = "", json = true, contentType: string | boolean = "application/json", headers: any = {}): Promise<T> {
         if (contentType) headers["Content-Type"] = contentType
 
